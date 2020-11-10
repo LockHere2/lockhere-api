df5629aa905ff345afeb5194ac24d08354c60d4a
Mateusspadacio <mateusspadacio@gmail.com>
2020-11-10 19:18:20 -0300

    correção falha de segurança random code

--- src/api/helpers/generateCode.js
+++ src/api/helpers/generateCode.js
@@ -1,1 +1,11 @@
-export default () => Math.floor(100000 + Math.random() * 900000);
+import crypto from 'crypto';
+
+export default () => {
+    let code = "";
+
+    do {
+        code += crypto.randomBytes(3).readUIntBE(0, 3);
+    } while (code.length < 6);
+
+    return code.slice(0, 6);
+}
