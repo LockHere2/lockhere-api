ad09dde66726e4e37a3d5926445bb66234cce713
Mateusspadacio <mateusspadacio@gmail.com>
2020-11-11 20:38:38 -0300

    correção esteira

--- src/app.js
+++ src/app.js
@@ -44,6 +44,7 @@ app.use((error, req, res, next) => {
 });
 
 app.listen(PORT, () => {
+  console.log('alterou testeetete');
   console.log(`Server is running at PORT http://localhost:${PORT}`);
 });
 
